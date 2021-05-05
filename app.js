963721273db9db079fb9bc5cba2c0fb3b11498fc
Mohamed-Elzohary <MohamedElzohary2001@outlook.com>
2021-05-05 04:40:26 +0200

    Slideshare: Your First 10 million Users on the AWS Cloud

--- app.js
+++ app.js
@@ -17,33 +17,35 @@ const download = (response) => {
 
   // NUMBERS CONFIG
   // SHOULD HAVE CHOICES FOR NUMBER FORMAT (1, 01, 001)
-  let imgs_nums = [
+  let imgs_nums_arr = [
     '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
     '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
     '21', '22', '23', '24', '25', '26', '27', '28', '29', '30'
   ]
 
+  const imgs_nums = 80;
+
   // FOR LOOP STARTS
-  for (let index = 0; index < imgs_nums.length; index++) {
+  for (let index = 0; index < imgs_nums; index++) {
     const element = imgs_nums[index];
 
-    const uri = `https://image.slidesharecdn.com/paradigmsofleadership-160122220040/95/paradigms-of-leadership-${imgs_nums[index]}-638.jpg?cb=1453500159`;
+    const uri = `https://image.slidesharecdn.com/yourfirst10millionusers-150323030827-conversion-gate01/95/your-first-10-million-users-on-the-aws-cloud-${index}-638.jpg?cb=1427098313`;
 
     request.head(uri, function (err, res, body) {
       console.log("content-type:", res.headers["content-type"]);
       console.log("content-length:", res.headers["content-length"]);
 
-      var f = fs.createWriteStream(`imgs/image-${imgs_nums[index]}.jpg`);
+      var f = fs.createWriteStream(`imgs/image-${index}.jpg`);
 
       f.on("finish", function () {
         // do stuff
-        console.log(`STREAM::WRITE::DONE__IMG::${imgs_nums[index]}`);
+        console.log(`STREAM::WRITE::DONE__IMG::${index}`);
       });
 
       request(uri)
         .pipe(f)
         .on("finish", () => {
-          console.log(`PIPE::DONE__IMG::${imgs_nums[index]}`);
+          console.log(`PIPE::DONE__IMG::${index}`);
         });
     });
   }

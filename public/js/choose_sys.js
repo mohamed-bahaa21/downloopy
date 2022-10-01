1c34bddb3c514298dd24d1cf5637a096971ba66e
Mohamed-Elzohary <MohamedElzohary2001@outlook.com>
2022-10-02 00:25:55 +0200

    added: selective files form

--- public/js/choose_sys.js
+++ public/js/choose_sys.js
@@ -20,6 +20,7 @@ var html_content = {
 
     selective: `
         <form action="/url_parser" method="post">
+            <!-- 
             <div class="form-item">
                 <label for="website">Choose a website:</label>
                 <select name="website" id="website">
@@ -30,8 +31,7 @@ var html_content = {
                     <option value="dar.bibalex">dar.bibalex</option>
                     <option value="yunong.io">yunong.io</option>
                     <option value="InfoQ">InfoQ</option>
-                </select>
-                
+                </select>    
             </div>
 
             <div class="form-item">
@@ -43,7 +43,6 @@ var html_content = {
                 
             </div>
 
-            <!-- 
             <div class="form-item">
                 <label for="FOLDER_NAME">Folder Name</label>
                 <input type="text" name="FOLDER_NAME" id="FOLDER_NAME">
@@ -63,11 +62,29 @@ var html_content = {
                 <label for="URI_END">URI_END</label>
                 <input type="text" name="URI_END" id="URI_END">
             </div>
-            --!>
+            -->
 
             <div class="form-item">
-                <label for="PAGES">PAGES</label>
-                <input type="text" name="PAGES" id="PAGES">
+                <label for="PAGES">Pages: </label>
+                <span id="selected_pages"></span>
+
+                <div style="display: flex">
+                    <input type="text" name="PAGES" id="pages_input">
+
+                    <span id="add_page_btn" class="overlay__btn overlay__btn--colors" 
+                    style="margin: 0 auto !important;
+                    width: fit-content !important;
+                    position: relative;
+                    left: -2.5rem;
+                    bottom: -0.12rem;
+                    border-top-left-radius: 0;
+                    border-bottom-left-radius: 0;
+                    font-size: 0.75em;
+                    padding: 0rem 0.6rem;
+                    scale: 1.075;">
+                        Add
+                    </span>
+                </div>
             </div>
 
             <div class="form-item">
@@ -87,12 +104,23 @@ var html_content = {
 
 var form_content = document.getElementById('form-content');
 var tags = document.getElementsByClassName('tag');
+var tag_cont = document.getElementsByClassName('tag-cont');
+
+for (let i = 0; i < tag_cont.length; i++) {
+    const element = tag_cont[i];
+    element.style.display = 'none';
+}
+
+let currTag = 'mixed';
+
 
 for (let i = 0; i < tags.length; i++) {
     const tag = tags[i];
 
     tag.addEventListener('click', () => {
-        form_content.innerHTML = html_content[tag.id];
+        form_content.children[currTag].style.display = 'none';
+        currTag = tag.id;
+        form_content.children[currTag].style.display = 'block';
         clearTagsActive();
         tag.classList.add('active');
     })
@@ -104,3 +132,14 @@ function clearTagsActive() {
         tag.classList.remove('active');
     }
 }
+
+// =======================
+var selected_pages = document.getElementById('selected_pages')
+var pages_input = document.getElementById('pages_input')
+var add_page_btn = document.getElementById('add_page_btn')
+
+add_page_btn.addEventListener('click', () => {
+    selected_pages.innerHTML += `<span class="page">${pages_input.value}</span>`;
+    pages_input.value = '';
+    console.log(selected_pages);
+})
